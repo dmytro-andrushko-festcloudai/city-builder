4f4e3f0ae4d83bc057725ce25681894ac73bc763
Dmytro Andrushko <dmytro.andrushko@festcloud.ai>
2024-05-22 11:51:05 +0300

    Remove about modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,21 @@
 import "./Header.css";
 import React, { useState } from "react";
 import LoadModal from "components/modals/LoadModal";
-import AboutModal from "components/modals/AboutModal";
 
 export default function Header({
   gridSize,
   onGridSizeChange,
   onRandomize,
   onClear,
-  onLoadHash
+  onLoadHash,
 }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
 
   return (
     <header>
       <div>
         <span className="title">IsoCity Builder</span>
         <button onClick={() => setIsModalOpen(true)}>Load...</button>
-        <button onClick={() => setIsAboutModalOpen(true)}>About</button>
       </div>
       <div className="options">
         <label htmlFor="gridsize-input">Grid Size: {gridSize}</label>&nbsp;
@@ -37,7 +34,6 @@ export default function Header({
       {isModalOpen && (
         <LoadModal setOpen={setIsModalOpen} onLoadHash={onLoadHash} />
       )}
-      {isAboutModalOpen && <AboutModal setOpen={setIsAboutModalOpen} />}
     </header>
   );
 }
