62ca5664b87be5edc3c967846c0e21e014a6e6d6
Dmytro Andrushko <dmytro.andrushko@festcloud.ai>
2024-05-22 10:40:42 +0300

    Updates

---
+++ precache-manifest.674779bf5e39d4eb61e763052a5a7358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5eda328799f502f5aeb02fb0edb603",
+    "url": "/city-builder/index.html"
+  },
+  {
+    "revision": "f4507e2f19a9bbce8d60",
+    "url": "/city-builder/static/css/main.5d8bebb8.chunk.css"
+  },
+  {
+    "revision": "9a5d6b7a9e0d149cc19e",
+    "url": "/city-builder/static/js/2.5074ac46.chunk.js"
+  },
+  {
+    "revision": "58258b32c9b200f153040c762fffa468",
+    "url": "/city-builder/static/js/2.5074ac46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4507e2f19a9bbce8d60",
+    "url": "/city-builder/static/js/main.18f2f08a.chunk.js"
+  },
+  {
+    "revision": "c20a4bc85d06b0008643",
+    "url": "/city-builder/static/js/runtime-main.b5ed8b15.js"
+  }
+]);
